a873c10631f975a87bc5ff66a2a911c725fc0c6b
carpenter-js <carpenterbarrett@gmail.com>
2018-12-13 10:03:49 -0700

    attatched a handler!

--- src/components/game.js
+++ src/components/game.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from './header';
 import GuessSection from './guess-section';
 import GuessCount  from './guess-count';
 import GuessList from './guess-list';
 
-export default function Game(props) {
-    return (
-        <div>
-            <Header />
-            <GuessSection feedback="Make your guess!" />
-            <GuessCount count={3} />
-            <GuessList guesses={[10, 15, 25]} />
-        </div>
-    );
+class Game extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentGuess: '',
+            recentGuess: '',
+            guessHistory: '',
+            currentFeedback: '',
+            // currentSolution: ''
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <Header />
+                <GuessSection feedback="Make your guess!" />
+                <GuessCount count={3} />
+                <GuessList guesses={[10, 15, 25]} />
+            </div>
+        );
+    }
+    
 }
+
+export default Game;
 
